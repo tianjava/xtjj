5c88c46610e5503a4dc3872569a867687938e014
tianjava <106210970+tianjava@users.noreply.github.com>
2022-08-22 16:23:12 +0800

    Initial commit

---
+++ scripts/utils/env.js
@@ -0,0 +1,9 @@
+const env = process.env || {};
+
+module.exports = {
+  EMAIL_USER: env.EMAIL_USER,
+  EMAIL_PASS: env.EMAIL_PASS,
+  EMAIL_TO: env.EMAIL_TO,
+  COOKIE: env.COOKIE,
+  DINGDING_WEBHOOK:env.DINGDING_WEBHOOK
+};
